62897c047bde3636beb58bd948472bb74efe95a6
Rich <r.neese@gmail.com>
2022-12-06 19:07:11 -0500

    updated my builds scripts
    added --depth 1

--- build-riscv-desktop-full.sh
+++ build-riscv-desktop-full.sh
@@ -2,15 +2,14 @@
 sudo rm -rf RISCVbian
 
 # uncommet the branch you want to use
-git clone -b risc-v https://github.com/150balbes/RISCVbian.git
+git clone --depth 1 https://github.com/150balbes/RISCVbian.git
 
 mkdir RISCVbian/userpatches
 
-
 cat > RISCVbian/userpatches/lib.config << DELIM
 NO_APT_CACHER=yes
-APT_PROXY_ADDR=206.82.251.133:3142
-NO_HOST_RELEASE_CHECK=yes
+#APT_PROXY_ADDR=206.82.251.133:3142
+#NO_HOST_RELEASE_CHECK=yes
 DELIM
 
 cd RISCVbian
