3e8f615d66c1086cb5c690d78ff3fcbca31164de
Rich <r.neese@gmail.com>
2022-12-12 08:55:20 -0500

    fix typo

--- build-riscv-test.sh
+++ build-riscv-test.sh
@@ -3,7 +3,7 @@ sudo rm -rf build
 
 # uncommet the branch you want to use
 
-git clone --depth 1 git clone --depth 1 -b starfive-v1-testing https://github.com/RichNeese/build.git
+git clone --depth 1 -b starfive-v1-testing https://github.com/RichNeese/build.git
 
 mkdir build/userpatches
 
